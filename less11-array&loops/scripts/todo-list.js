e4b4d3f97b9bcb1ade2c84bdb99033be6a6e3f66
Liu <Andy.Liu3@ontario.ca>
2024-12-11 14:38:24 -0500

    half completed less 11 part2

--- less11-array&loops/scripts/todo-list.js
+++ less11-array&loops/scripts/todo-list.js
@@ -1,11 +1,41 @@
-const todoList = [];
+const todoList = [{
+    name: 'make dinner',
+    duedate: '2022-12-22'
+}, {
+    name: 'wash dishes',
+    duedate: '2022-12-22'
+}];
+
+renderTodoList();
+
+function renderTodoList() {
+    let html = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        const todoName = todoObject.name;
+        const dueDate = todoObject.duedate;
+        html += `
+                <div>${todoName}</div> 
+                <div>${dueDate}</div>
+                <button class="delete-button" onclick="
+                    todoList.splice(${i}, 1);
+                    renderTodoList();
+                ">Delete</button>
+        `;
+    }
+    
+    document.querySelector('.js-result1').innerHTML = html;
+}
 
 function addTodo() {
     const todoname = document.querySelector('.js-input1').value;
-    todoList.push(todoname);
-    console.log(todoList);
+    const dueDate = document.querySelector('.Duedate').value;
+    todoList.push({
+        name: todoname, 
+        duedate: dueDate
+    });
 
     document.querySelector('.js-input1').value = '';
-
-    document.querySelector('.js-result1').innerHTML += `<p>${todoname}</p>`;
+    renderTodoList();
 }
